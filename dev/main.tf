3a9775c346669da11be76616ebba72b9a9cffa07
zubeirahamed <126669677+zubeirahamed@users.noreply.github.com>
2023-09-17 13:14:00 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -2,11 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "terra-grunt-dev" {
-  ami           = "ami-053b0d53c279acc90"  
+resource "aws_instance" "dev-inst-grunt" {
+  ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
-
-  tags = {
-    Name = "terra-grunt-instance-dev"
-  }
 }
