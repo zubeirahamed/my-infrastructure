555c931d7383fd2dcc33db454b367b1a3c746ce4
zubeirahamed <126669677+zubeirahamed@users.noreply.github.com>
2023-09-17 19:54:24 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-1"
 }
 
 resource "aws_instance" "dev-inst-grunt" {
-  ami           = "ami-053b0d53c279acc90"
+  ami           = "ami-0f8e81a3da6e2510a"
   instance_type = "t2.micro"
   tags = {
           Name = "dev-inst-grunt"
@@ -14,7 +14,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terragrunt-bucket1"
     key            = "terraform-states/dev/terraform.tfstate"
-    region         = "us-east-1"  # Your AWS region
+    region         = "us-west-1"  # Your AWS region
     encrypt        = true
   }
 }
