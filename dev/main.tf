0b7f1d58d66719f68e3d9cba69830023a4688985
zubeirahamed <126669677+zubeirahamed@users.noreply.github.com>
2023-09-17 17:58:56 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -5,6 +5,9 @@ provider "aws" {
 resource "aws_instance" "dev-inst-grunt" {
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
+  tags = {
+          Name = "dev-inst-grunt"
+}
 }
 
 terraform {
