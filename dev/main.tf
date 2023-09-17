03e0756370162936d16dbcffe0bc77cf11233ba7
zubeirahamed <126669677+zubeirahamed@users.noreply.github.com>
2023-09-17 17:43:27 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -6,3 +6,12 @@ resource "aws_instance" "dev-inst-grunt" {
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "your-s3-bucket-name"
+    key            = "path/to/terraform.tfstate"
+    region         = "us-east-1"  # Your AWS region
+    encrypt        = true
+  }
+}
