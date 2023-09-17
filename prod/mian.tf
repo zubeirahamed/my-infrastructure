f0aa158b4b72d4556089501c827179c467ef5dfa
zubeirahamed <126669677+zubeirahamed@users.noreply.github.com>
2023-09-17 17:52:02 +0530

    Update mian.tf

--- prod/mian.tf
+++ prod/mian.tf
@@ -6,3 +6,12 @@ resource "aws_instance" "prod-inst-grunt" {
   ami           = "ami-024e6efaf93d85776"
   instance_type = "t2.micro"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "my-terragrunt-bucket1"
+    key            = "path/to/terraform.tfstate"
+    region         = "us-east-1"  # Your AWS region
+    encrypt        = true
+  }
+}
