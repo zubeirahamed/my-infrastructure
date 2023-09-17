de21a97b173408df279a87c4a8654475e7cecf7d
zubeirahamed <126669677+zubeirahamed@users.noreply.github.com>
2023-09-17 17:59:31 +0530

    Update mian.tf

--- prod/mian.tf
+++ prod/mian.tf
@@ -5,6 +5,9 @@ provider "aws" {
 resource "aws_instance" "prod-inst-grunt" {
   ami           = "ami-024e6efaf93d85776"
   instance_type = "t2.micro"
+  tags = {
+          Name = "prod-inst-grunt"
+}
 }
 
 terraform {
